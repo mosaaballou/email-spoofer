1650e8328341c1d4e1c2733e13b563cf2db3748b
mosaaballou <48595478+mosaaballou@users.noreply.github.com>
2020-02-19 18:07:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<!--
+###############################
+* CREATED BY Hak9        *
+* xHak9x                 *
+###############################
+-->
+
+<?php
+if (isset($_POST['ajax'])) {
+$to = $_POST['to'];
+$subject = $_POST['sub'];
+$msg = $_POST['msg'];
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: ".$_POST['name']."<".$_POST['from'].">";
+
+$send = mail($to,$subject,$msg,$headers);
+
+if ($send) {
+	echo "<p id='success'>✔️  $to</p>";
+}else{
+	echo "<p id='error'>❌  $to</p>";
+}
+exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -103,7 +129,7 @@
 	</style>
 </head>
 <body>
-<form action="mailer.php" method="post">
+<form action="" method="post">
 <div class="main" style="margin-top: 100px;">
 	<h1 id="title">Email Spoofer - Hak9</h1>
 	<div>
